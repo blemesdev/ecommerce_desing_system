7b1d306f3942295f09719831ef5f0b758f40693a
Bruno Lemes Dos Santos <112427743+brunosantos319@users.noreply.github.com>
2024-03-31 20:22:49 -0300

    Update button_login_facebook.dart

--- lib/presenter/widgets/buttons/button_login_facebook.dart
+++ lib/presenter/widgets/buttons/button_login_facebook.dart
@@ -1,11 +1,12 @@
 import 'package:ecommerce_design_system/domain/interfaces/button_login_abstract.dart';
 
 class ButtonLoginFacebook extends ButtonLoginSocial {
-  const ButtonLoginFacebook(
-      {super.key,
-      required super.name,
-      required super.icon,
-      required super.onPressed,
-      required super.child,
-      required super.style});
+  const ButtonLoginFacebook({
+    super.key,
+    required super.name,
+    required super.icon,
+    required super.onPressed,
+    required super.child,
+    required super.style,
+  });
 }
