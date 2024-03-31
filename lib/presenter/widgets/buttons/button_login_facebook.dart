039f0756b27308b44e9aee2839b828fad9775167
Bruno Lemes Dos Santos <112427743+brunosantos319@users.noreply.github.com>
2024-03-31 20:19:36 -0300

    refactor: add positional parameters in button

--- lib/presenter/widgets/buttons/button_login_facebook.dart
+++ lib/presenter/widgets/buttons/button_login_facebook.dart
@@ -1,15 +1,11 @@
 import 'package:ecommerce_design_system/domain/interfaces/button_login_abstract.dart';
-import 'package:flutter/material.dart';
 
 class ButtonLoginFacebook extends ButtonLoginSocial {
-  ButtonLoginFacebook({super.key})
-      : super(
-          icon: const Icon(Icons.facebook),
-          child: const Text('Facebook'),
-          name: 'Facebook',
-          style: const ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll(Colors.blue),
-          ),
-          onPressed: () {},
-        );
+  const ButtonLoginFacebook(
+      {super.key,
+      required super.name,
+      required super.icon,
+      required super.onPressed,
+      required super.child,
+      required super.style});
 }
